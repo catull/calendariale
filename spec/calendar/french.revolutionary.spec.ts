d608f6e312b0bf9d0027fb935f9a1a1e40f4a9c0
Carlo Dapor <catull@gmail.com>
2017-02-11 22:13:17 +0100

    Improve spec tests.

--- spec/calendar/french.revolutionary.spec.ts
+++ spec/calendar/french.revolutionary.spec.ts
@@ -49,34 +49,35 @@ const data3 = [
 ];
 
 describe ('French Revolutionary calendar spec', () => {
-  let date, expected, actual, decade, jour;
+  let date, expected, actual, julian, decade, jour;
 
   it ('should convert a French Revolutionary date to Julian day', () => {
-    data3.forEach ((data) => {
-      date     = data.french;
-      expected = data.rataDie + Const.J0000;
-      jour     = date.day;
-      decade   = Math.floor ((jour - 1) / 10) + 1;
-      jour     = astro.amod (jour, 10);
-      actual   = cal.toJdn (date.year, date.month, decade, jour);
-      expect (expected).to.be.equal (actual);
+    data3.forEach (dt => {
+      date   = dt.french;
+      julian = dt.rataDie + Const.J0000;
+      jour   = date.day;
+      decade = Math.floor ((jour - 1) / 10) + 1;
+      jour   = astro.amod (jour, 10);
+      actual = cal.toJdn (date.year, date.month, decade, jour);
+      expect (julian).to.be.equal (actual);
     });
   });
 
   it ('should convert a Julian day to a French Revolutionary date', () => {
-    data3.forEach ((data) => {
-      date     = data.french;
+    data3.forEach (dt => {
+      julian   = dt.rataDie + Const.J0000;
+      date     = dt.french;
       jour     = date.day;
       decade   = Math.floor ((jour - 1) / 10) + 1;
       jour     = astro.amod (jour, 10);
-      expected = { year: date.year, month: date.month, decade: decade, day: jour };
-      actual   = cal.fromJdn (data.rataDie + Const.J0000);
+      expected = { jdn: julian, year: date.year, month: date.month, decade: decade, day: jour };
+      actual   = cal.fromJdn (julian);
 
-      // expect (expected).to.be.eql (actual);
-      expect (expected.year).to.be.equal (actual.year);
-      expect (expected.month).to.be.equal (actual.month);
-      expect (expected.decade).to.be.equal (actual.decade);
-      expect (expected.day).to.be.equal (actual.day);
+      expect (expected).to.be.eql (actual);
+      // expect (expected.year).to.be.equal (actual.year);
+      // expect (expected.month).to.be.equal (actual.month);
+      // expect (expected.decade).to.be.equal (actual.decade);
+      // expect (expected.day).to.be.equal (actual.day);
     });
   });
 
