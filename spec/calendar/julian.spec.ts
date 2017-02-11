a5b4f4a460508c0f60c5b6281014f19cfdcba826
Carlo Dapor <catull@gmail.com>
2017-02-11 23:17:21 +0100

    Improve spec tests.

--- spec/calendar/julian.spec.ts
+++ spec/calendar/julian.spec.ts
@@ -50,34 +50,33 @@ describe ('Julian calendar spec', () => {
   let date, expected, actual;
 
   it ('should convert a Julian date to Julian day', () => {
-    data1.forEach ((data) => {
-      date = data.julianDate;
-      expected = data.julianDay;
+    data1.forEach (dt => {
+      date   = dt.julianDate;
       actual = cal.toJdn (date.year, date.month, date.day);
 
-      expect (expected).to.be.equal (actual);
+      expect (dt.julianDay).to.be.equal (actual);
     });
   });
 
   it ('should convert a Julian day to a Julian date', () => {
-    data1.forEach ((data) => {
-      date = data.julianDate;
-      expected = { year: date.year, month: date.month, day: date.day };
-      actual = cal.fromJdn (data.julianDay);
+    data1.forEach (dt => {
+      date     = dt.julianDate;
+      expected = { 'jdn': dt.julianDay, 'year': date.year, 'month': date.month, 'day': date.day, 'yearLeap': cal.isLeapYear (date.year) };
+      actual   = cal.fromJdn (dt.julianDay);
 
-      // expect (expected).to.be.eql (actual);
-      expect (expected.year).to.be.equal (actual.year);
-      expect (expected.month).to.be.equal (actual.month);
-      expect (expected.day).to.be.equal (actual.day);
+      expect (expected).to.be.eql (actual);
+      // expect (expected.year).to.be.equal (actual.year);
+      // expect (expected.month).to.be.equal (actual.month);
+      // expect (expected.day).to.be.equal (actual.day);
     });
   });
 
   it ('should determine whether a Julian year is leap year', () => {
-    [ 4, 20, 1600, 1700, 1760, 1800, 1840, 1904, 1980, 2000 ].forEach ((year) => {
+    [ 4, 20, 1600, 1700, 1760, 1800, 1840, 1904, 1980, 2000 ].forEach (year => {
       expect (cal.isLeapYear (year)).to.be.equal (true);
     });
 
-    [ 1, 2, 3, 5, 1599, 1970, 2001 ].forEach ((year) => {
+    [ 1, 2, 3, 5, 1599, 1970, 2001 ].forEach (year => {
       expect (cal.isLeapYear (year)).to.be.equal (false);
     });
   });
