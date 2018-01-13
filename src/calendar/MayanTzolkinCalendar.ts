86f96605c2c8328c84015300bcf4a6e33d379991
Carlo Dapor <catull@gmail.com>
2018-01-13 19:23:45 +0100

    Corrected source code of src/calendar/MayanTzolkinCalendar.ts to follow tslint stricter mode.

--- src/calendar/MayanTzolkinCalendar.ts
+++ src/calendar/MayanTzolkinCalendar.ts
@@ -3,14 +3,15 @@ import { mayan } from '../Const';
 import { MonthCalendar } from '../Calendar';
 
 export class MayanTzolkinCalendar extends MonthCalendar {
-  constructor(jdn: number, month: number, day: number) {
-    super(jdn, month, day);
-  }
-
   // Calculate Mayan Tzolkin calendar date from Julian day
   public static fromJdn(jdn: number): MayanTzolkinCalendar {
     const count: number = Math.floor(jdn - 0.5) + 0.5 - mayan.EPOCH;
 
     return new MayanTzolkinCalendar(jdn, amod(count + 4, 13), amod(count, 20));
   }
+
+  constructor(jdn: number, month: number, day: number) {
+    super(jdn, month, day);
+  }
+
 }
