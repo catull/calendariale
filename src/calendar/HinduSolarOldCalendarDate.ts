c4d2298193488de476f7fd561de15dfc996cb191
Carlo Dapor <catull@gmail.com>
2018-12-14 22:51:01 +0100

    Renamed all classes *CalendarDate -> *Date

--- src/calendar/HinduSolarOldCalendarDate.ts
+++
@@ -1,9 +0,0 @@
-import { YearMonthCalendarDate } from './core';
-
-export class HinduSolarOldCalendarDate extends YearMonthCalendarDate {
-
-  constructor(jdn: number, year: number, month: number, day: number) {
-    super(jdn, year, month, day);
-  }
-
-}
--- src/calendar/MayanTzolkinCalendarDate.ts
+++
@@ -1,9 +0,0 @@
-import { MonthCalendarDate } from './core';
-
-export class MayanTzolkinCalendarDate extends MonthCalendarDate {
-
-  constructor(jdn: number, month: number, day: number) {
-    super(jdn, month, day);
-  }
-
-}
