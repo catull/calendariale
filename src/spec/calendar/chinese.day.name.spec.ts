c1c93a763cdf1fb00437f6cd6c8323a3fc592fac
Carlo Dapor <catull@gmail.com>
2018-12-14 23:52:02 +0100

    Cleaned up Chinese Day Name calendar spec.

--- src/spec/calendar/chinese.day.name.spec.ts
+++ src/spec/calendar/chinese.day.name.spec.ts
@@ -2,54 +2,48 @@ import { J0000 } from '../../Const';
 
 import { ChineseDayNameCalendar as cal } from '../../calendar/ChineseDayNameCalendar';
 
-const data4 = [
-  { 'rataDie': -214193, 'chineseDayName': { 'stem':  2, 'branch': 10 } },
-  { 'rataDie':  -61387, 'chineseDayName': { 'stem':  8, 'branch':  8 } },
-  { 'rataDie':   25469, 'chineseDayName': { 'stem':  4, 'branch':  8 } },
-  { 'rataDie':   49217, 'chineseDayName': { 'stem':  2, 'branch':  8 } },
-  { 'rataDie':  171307, 'chineseDayName': { 'stem':  2, 'branch': 10 } },
-  { 'rataDie':  210155, 'chineseDayName': { 'stem': 10, 'branch':  2 } },
-  { 'rataDie':  253427, 'chineseDayName': { 'stem':  2, 'branch':  2 } },
-  { 'rataDie':  369740, 'chineseDayName': { 'stem':  5, 'branch': 11 } },
-  { 'rataDie':  400085, 'chineseDayName': { 'stem': 10, 'branch':  8 } },
-  { 'rataDie':  434355, 'chineseDayName': { 'stem': 10, 'branch':  6 } },
-  { 'rataDie':  452605, 'chineseDayName': { 'stem': 10, 'branch':  4 } },
-  { 'rataDie':  470160, 'chineseDayName': { 'stem':  5, 'branch':  3 } },
-  { 'rataDie':  473837, 'chineseDayName': { 'stem':  2, 'branch':  8 } },
-  { 'rataDie':  507850, 'chineseDayName': { 'stem':  5, 'branch':  1 } },
-  { 'rataDie':  524156, 'chineseDayName': { 'stem':  1, 'branch': 11 } },
-  { 'rataDie':  544676, 'chineseDayName': { 'stem':  1, 'branch': 11 } },
-  { 'rataDie':  567118, 'chineseDayName': { 'stem':  3, 'branch':  1 } },
-  { 'rataDie':  569477, 'chineseDayName': { 'stem':  2, 'branch':  8 } },
-  { 'rataDie':  601716, 'chineseDayName': { 'stem':  1, 'branch':  3 } },
-  { 'rataDie':  613424, 'chineseDayName': { 'stem':  9, 'branch': 11 } },
-  { 'rataDie':  626596, 'chineseDayName': { 'stem':  1, 'branch':  7 } },
-  { 'rataDie':  645554, 'chineseDayName': { 'stem':  9, 'branch':  5 } },
-  { 'rataDie':  664224, 'chineseDayName': { 'stem':  9, 'branch':  3 } },
-  { 'rataDie':  671401, 'chineseDayName': { 'stem':  6, 'branch':  4 } },
-  { 'rataDie':  694799, 'chineseDayName': { 'stem':  4, 'branch':  2 } },
-  { 'rataDie':  704424, 'chineseDayName': { 'stem':  9, 'branch':  3 } },
-  { 'rataDie':  708842, 'chineseDayName': { 'stem':  7, 'branch':  5 } },
-  { 'rataDie':  709409, 'chineseDayName': { 'stem':  4, 'branch':  8 } },
-  { 'rataDie':  709580, 'chineseDayName': { 'stem':  5, 'branch': 11 } },
-  { 'rataDie':  727274, 'chineseDayName': { 'stem':  9, 'branch':  5 } },
-  { 'rataDie':  728714, 'chineseDayName': { 'stem':  9, 'branch':  5 } },
-  { 'rataDie':  744313, 'chineseDayName': { 'stem':  8, 'branch':  4 } },
-  { 'rataDie':  764652, 'chineseDayName': { 'stem':  7, 'branch':  3 } }
+const dates = [
+  { rataDie: -214193, date: { stem:  2, branch: 10 } },
+  { rataDie:  -61387, date: { stem:  8, branch:  8 } },
+  { rataDie:   25469, date: { stem:  4, branch:  8 } },
+  { rataDie:   49217, date: { stem:  2, branch:  8 } },
+  { rataDie:  171307, date: { stem:  2, branch: 10 } },
+  { rataDie:  210155, date: { stem: 10, branch:  2 } },
+  { rataDie:  253427, date: { stem:  2, branch:  2 } },
+  { rataDie:  369740, date: { stem:  5, branch: 11 } },
+  { rataDie:  400085, date: { stem: 10, branch:  8 } },
+  { rataDie:  434355, date: { stem: 10, branch:  6 } },
+  { rataDie:  452605, date: { stem: 10, branch:  4 } },
+  { rataDie:  470160, date: { stem:  5, branch:  3 } },
+  { rataDie:  473837, date: { stem:  2, branch:  8 } },
+  { rataDie:  507850, date: { stem:  5, branch:  1 } },
+  { rataDie:  524156, date: { stem:  1, branch: 11 } },
+  { rataDie:  544676, date: { stem:  1, branch: 11 } },
+  { rataDie:  567118, date: { stem:  3, branch:  1 } },
+  { rataDie:  569477, date: { stem:  2, branch:  8 } },
+  { rataDie:  601716, date: { stem:  1, branch:  3 } },
+  { rataDie:  613424, date: { stem:  9, branch: 11 } },
+  { rataDie:  626596, date: { stem:  1, branch:  7 } },
+  { rataDie:  645554, date: { stem:  9, branch:  5 } },
+  { rataDie:  664224, date: { stem:  9, branch:  3 } },
+  { rataDie:  671401, date: { stem:  6, branch:  4 } },
+  { rataDie:  694799, date: { stem:  4, branch:  2 } },
+  { rataDie:  704424, date: { stem:  9, branch:  3 } },
+  { rataDie:  708842, date: { stem:  7, branch:  5 } },
+  { rataDie:  709409, date: { stem:  4, branch:  8 } },
+  { rataDie:  709580, date: { stem:  5, branch: 11 } },
+  { rataDie:  727274, date: { stem:  9, branch:  5 } },
+  { rataDie:  728714, date: { stem:  9, branch:  5 } },
+  { rataDie:  744313, date: { stem:  8, branch:  4 } },
+  { rataDie:  764652, date: { stem:  7, branch:  3 } },
 ];
 
 describe ('Chinese Day Name calendar spec', () => {
-  let date;
-  let expected;
-  let actual;
-  let julian;
-
   it ('should convert a Julian day number to a Chinese Day Name date', () => {
-    data4.forEach (dt => {
-      julian   = dt.rataDie + J0000;
-      date     = dt.chineseDayName;
-      expected = { jdn: julian, stem: date.stem, branch: date.branch };
-      actual   = cal.fromJdn (julian);
+    dates.forEach (({ rataDie, date }) => {
+      const jdn      = rataDie + J0000;
+      const expected = { jdn, ...date };
+      const actual   = cal.fromJdn (jdn);
 
       expect (expected).toEqual (actual);
       expect (expected.stem).toBe (actual.getStem());
