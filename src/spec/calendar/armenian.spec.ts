d1314d70e6b4caf31447e8c1ed25ee7a246adf09
Carlo Dapor <catull@gmail.com>
2018-12-14 23:14:28 +0100

    Cleaned up Armenian calendar spec.

--- src/spec/calendar/armenian.spec.ts
+++ src/spec/calendar/armenian.spec.ts
@@ -1,67 +1,62 @@
 import { INVALID_DAY, INVALID_MONTH } from '../../Const';
 import { ArmenianCalendar as cal } from '../../calendar/ArmenianCalendar';
+import { ArmenianDate } from '../../calendar';
 
 const data1 = [
-  { 'julianDay': 1507231.5, 'armenian': { 'year': -1138, 'month':  4, 'day': 10 } },
-  { 'julianDay': 1660037.5, 'armenian': { 'year':  -720, 'month': 12, 'day':  6 } },
-  { 'julianDay': 1746893.5, 'armenian': { 'year':  -482, 'month': 11, 'day': 22 } },
-  { 'julianDay': 1770641.5, 'armenian': { 'year':  -417, 'month': 12, 'day': 15 } },
-  { 'julianDay': 1892731.5, 'armenian': { 'year':   -82, 'month':  6, 'day': 10 } },
-  { 'julianDay': 1931579.5, 'armenian': { 'year':    24, 'month': 11, 'day': 18 } },
-  { 'julianDay': 1974851.5, 'armenian': { 'year':   143, 'month':  6, 'day':  5 } },
-  { 'julianDay': 2091164.5, 'armenian': { 'year':   462, 'month':  2, 'day':  3 } },
-  { 'julianDay': 2121509.5, 'armenian': { 'year':   545, 'month':  3, 'day': 23 } },
-  { 'julianDay': 2155779.5, 'armenian': { 'year':   639, 'month':  2, 'day': 13 } },
-  { 'julianDay': 2174029.5, 'armenian': { 'year':   689, 'month':  2, 'day': 13 } },
-  { 'julianDay': 2191584.5, 'armenian': { 'year':   737, 'month':  3, 'day': 18 } },
-  { 'julianDay': 2195261.5, 'armenian': { 'year':   747, 'month':  4, 'day': 15 } },
-  { 'julianDay': 2229274.5, 'armenian': { 'year':   840, 'month':  6, 'day': 23 } },
-  { 'julianDay': 2245580.5, 'armenian': { 'year':   885, 'month':  2, 'day': 24 } },
-  { 'julianDay': 2266100.5, 'armenian': { 'year':   941, 'month':  5, 'day': 14 } },
-  { 'julianDay': 2288542.5, 'armenian': { 'year':  1002, 'month': 11, 'day': 11 } },
-  { 'julianDay': 2290901.5, 'armenian': { 'year':  1009, 'month':  4, 'day': 25 } },
-  { 'julianDay': 2323140.5, 'armenian': { 'year':  1097, 'month':  8, 'day': 24 } },
-  { 'julianDay': 2334848.5, 'armenian': { 'year':  1129, 'month':  9, 'day': 22 } },
-  { 'julianDay': 2348020.5, 'armenian': { 'year':  1165, 'month': 10, 'day': 24 } },
-  { 'julianDay': 2366978.5, 'armenian': { 'year':  1217, 'month': 10, 'day':  2 } },
-  { 'julianDay': 2385648.5, 'armenian': { 'year':  1268, 'month': 11, 'day': 27 } },
-  { 'julianDay': 2392825.5, 'armenian': { 'year':  1288, 'month':  7, 'day': 24 } },
-  { 'julianDay': 2416223.5, 'armenian': { 'year':  1352, 'month':  9, 'day':  2 } },
-  { 'julianDay': 2425848.5, 'armenian': { 'year':  1379, 'month':  1, 'day': 12 } },
-  { 'julianDay': 2430266.5, 'armenian': { 'year':  1391, 'month':  2, 'day': 20 } },
-  { 'julianDay': 2430833.5, 'armenian': { 'year':  1392, 'month':  9, 'day': 12 } },
-  { 'julianDay': 2431004.5, 'armenian': { 'year':  1393, 'month':  2, 'day': 28 } },
-  { 'julianDay': 2448698.5, 'armenian': { 'year':  1441, 'month':  8, 'day': 22 } },
-  { 'julianDay': 2450138.5, 'armenian': { 'year':  1445, 'month':  8, 'day':  2 } },
-  { 'julianDay': 2465737.5, 'armenian': { 'year':  1488, 'month':  4, 'day': 26 } },
-  { 'julianDay': 2486076.5, 'armenian': { 'year':  1544, 'month':  1, 'day': 15 } }
+  { jdn: 1507231.5, date: { year: -1138, month:  4, day: 10 } },
+  { jdn: 1660037.5, date: { year:  -720, month: 12, day:  6 } },
+  { jdn: 1746893.5, date: { year:  -482, month: 11, day: 22 } },
+  { jdn: 1770641.5, date: { year:  -417, month: 12, day: 15 } },
+  { jdn: 1892731.5, date: { year:   -82, month:  6, day: 10 } },
+  { jdn: 1931579.5, date: { year:    24, month: 11, day: 18 } },
+  { jdn: 1974851.5, date: { year:   143, month:  6, day:  5 } },
+  { jdn: 2091164.5, date: { year:   462, month:  2, day:  3 } },
+  { jdn: 2121509.5, date: { year:   545, month:  3, day: 23 } },
+  { jdn: 2155779.5, date: { year:   639, month:  2, day: 13 } },
+  { jdn: 2174029.5, date: { year:   689, month:  2, day: 13 } },
+  { jdn: 2191584.5, date: { year:   737, month:  3, day: 18 } },
+  { jdn: 2195261.5, date: { year:   747, month:  4, day: 15 } },
+  { jdn: 2229274.5, date: { year:   840, month:  6, day: 23 } },
+  { jdn: 2245580.5, date: { year:   885, month:  2, day: 24 } },
+  { jdn: 2266100.5, date: { year:   941, month:  5, day: 14 } },
+  { jdn: 2288542.5, date: { year:  1002, month: 11, day: 11 } },
+  { jdn: 2290901.5, date: { year:  1009, month:  4, day: 25 } },
+  { jdn: 2323140.5, date: { year:  1097, month:  8, day: 24 } },
+  { jdn: 2334848.5, date: { year:  1129, month:  9, day: 22 } },
+  { jdn: 2348020.5, date: { year:  1165, month: 10, day: 24 } },
+  { jdn: 2366978.5, date: { year:  1217, month: 10, day:  2 } },
+  { jdn: 2385648.5, date: { year:  1268, month: 11, day: 27 } },
+  { jdn: 2392825.5, date: { year:  1288, month:  7, day: 24 } },
+  { jdn: 2416223.5, date: { year:  1352, month:  9, day:  2 } },
+  { jdn: 2425848.5, date: { year:  1379, month:  1, day: 12 } },
+  { jdn: 2430266.5, date: { year:  1391, month:  2, day: 20 } },
+  { jdn: 2430833.5, date: { year:  1392, month:  9, day: 12 } },
+  { jdn: 2431004.5, date: { year:  1393, month:  2, day: 28 } },
+  { jdn: 2448698.5, date: { year:  1441, month:  8, day: 22 } },
+  { jdn: 2450138.5, date: { year:  1445, month:  8, day:  2 } },
+  { jdn: 2465737.5, date: { year:  1488, month:  4, day: 26 } },
+  { jdn: 2486076.5, date: { year:  1544, month:  1, day: 15 } },
 ];
 
 describe ('Armenian calendar spec', () => {
-  let date;
-  let expected;
-  let actual;
-
   it ('should convert an Armenian date to Julian day', () => {
     data1.forEach (dt => {
-      date = dt.armenian;
-      expected = dt.julianDay;
-      actual = cal.toJdn (date.year, date.month, date.day);
+      const { jdn, date } = dt;
+      const actual = cal.toJdn (date.year, date.month, date.day);
 
-      expect (expected).toBe (actual);
+      expect (jdn).toBe (actual);
     });
   });
 
   it ('should convert a Julian day to an Armenian date', () => {
     data1.forEach (dt => {
-      date = dt.armenian;
-      expected = { jdn: dt.julianDay, year: date.year, month: date.month, day: date.day };
-      actual = cal.fromJdn (dt.julianDay);
+      const { jdn, date } = dt;
+      const actual: ArmenianDate = cal.fromJdn (jdn);
 
-      expect (expected).toEqual (actual);
-      // expect (expected.year).toBe (actual.year);
-      // expect (expected.month).toBe (actual.month);
-      // expect (expected.day).toBe (actual.day);
+      expect ({ jdn, ...date }).toEqual (actual);
+      expect (date.year).toBe (actual.getYear());
+      expect (date.month).toBe (actual.getMonth());
+      expect (date.day).toBe (actual.getDay());
     });
   });
 
