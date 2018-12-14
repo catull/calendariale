63b84f1737919a6076f69b80d55e69354225db05
Carlo Dapor <catull@gmail.com>
2018-12-14 22:51:01 +0100

    Renamed all classes *CalendarDate -> *Date

--- src/calendar/HinduLunarOldCalendarDate.ts
+++
@@ -1,9 +0,0 @@
-import { LeapMonthCalendarDate } from './core';
-
-export class HinduLunarOldCalendarDate extends LeapMonthCalendarDate {
-
-  constructor(jdn: number, year: number, month: number, monthLeap: boolean, day: number) {
-    super(jdn, year, month, day, monthLeap);
-  }
-
-}
