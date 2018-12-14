d3f2a416d5ad8451f3500aebcd4e013612ceaf7a
Carlo Dapor <catull@gmail.com>
2018-12-15 00:16:13 +0100

    Cleaned up French Arithmetic calendar spec.

--- src/spec/calendar/french.arithmetic.spec.ts
+++ src/spec/calendar/french.arithmetic.spec.ts
@@ -2,68 +2,63 @@ import { INVALID_DAY, INVALID_MONTH, J0000 } from '../../Const';
 
 import { FrenchArithmeticCalendar as cal } from '../../calendar/FrenchArithmeticCalendar';
 
-const data3 = [
-  { 'rataDie': -214193, 'frenchArithmetic': { 'year': -2378, 'month': 11, 'day':  4 } },
-  { 'rataDie':  -61387, 'frenchArithmetic': { 'year': -1959, 'month':  3, 'day': 13 } },
-  { 'rataDie':   25469, 'frenchArithmetic': { 'year': -1721, 'month':  1, 'day':  2 } },
-  { 'rataDie':   49217, 'frenchArithmetic': { 'year': -1656, 'month':  1, 'day': 10 } },
-  { 'rataDie':  171307, 'frenchArithmetic': { 'year': -1322, 'month':  4, 'day': 18 } },
-  { 'rataDie':  210155, 'frenchArithmetic': { 'year': -1216, 'month':  9, 'day':  1 } },
-  { 'rataDie':  253427, 'frenchArithmetic': { 'year': -1097, 'month':  2, 'day': 19 } },
-  { 'rataDie':  369740, 'frenchArithmetic': { 'year':  -779, 'month':  8, 'day':  4 } },
-  { 'rataDie':  400085, 'frenchArithmetic': { 'year':  -696, 'month':  9, 'day':  5 } },
-  { 'rataDie':  434355, 'frenchArithmetic': { 'year':  -602, 'month':  7, 'day':  1 } },
-  { 'rataDie':  452605, 'frenchArithmetic': { 'year':  -552, 'month':  6, 'day': 20 } },
-  { 'rataDie':  470160, 'frenchArithmetic': { 'year':  -504, 'month':  7, 'day': 13 } },
-  { 'rataDie':  473837, 'frenchArithmetic': { 'year':  -494, 'month':  8, 'day':  8 } },
-  { 'rataDie':  507850, 'frenchArithmetic': { 'year':  -401, 'month':  9, 'day': 23 } },
-  { 'rataDie':  524156, 'frenchArithmetic': { 'year':  -356, 'month':  5, 'day': 13 } },
-  { 'rataDie':  544676, 'frenchArithmetic': { 'year':  -300, 'month':  7, 'day': 19 } },
-  { 'rataDie':  567118, 'frenchArithmetic': { 'year':  -239, 'month':  0, 'day':  1 } },
-  { 'rataDie':  569477, 'frenchArithmetic': { 'year':  -232, 'month':  6, 'day': 14 } },
-  { 'rataDie':  601716, 'frenchArithmetic': { 'year':  -144, 'month':  9, 'day': 22 } },
-  { 'rataDie':  613424, 'frenchArithmetic': { 'year':  -112, 'month': 10, 'day': 12 } },
-  { 'rataDie':  626596, 'frenchArithmetic': { 'year':   -76, 'month': 11, 'day':  6 } },
-  { 'rataDie':  645554, 'frenchArithmetic': { 'year':   -24, 'month': 10, 'day':  1 } },
-  { 'rataDie':  664224, 'frenchArithmetic': { 'year':    27, 'month': 11, 'day': 14 } },
-  { 'rataDie':  671401, 'frenchArithmetic': { 'year':    47, 'month':  7, 'day':  6 } },
-  { 'rataDie':  694799, 'frenchArithmetic': { 'year':   111, 'month':  7, 'day': 29 } },
-  { 'rataDie':  704424, 'frenchArithmetic': { 'year':   137, 'month': 12, 'day':  7 } },
-  { 'rataDie':  708842, 'frenchArithmetic': { 'year':   150, 'month':  1, 'day':  7 } },
-  { 'rataDie':  709409, 'frenchArithmetic': { 'year':   151, 'month':  7, 'day': 29 } },
-  { 'rataDie':  709580, 'frenchArithmetic': { 'year':   152, 'month':  1, 'day': 15 } },
-  { 'rataDie':  727274, 'frenchArithmetic': { 'year':   200, 'month':  6, 'day': 27 } },
-  { 'rataDie':  728714, 'frenchArithmetic': { 'year':   204, 'month':  6, 'day':  7 } },
-  { 'rataDie':  744313, 'frenchArithmetic': { 'year':   247, 'month':  2, 'day': 20 } },
-  { 'rataDie':  764652, 'frenchArithmetic': { 'year':   302, 'month': 11, 'day':  1 } }
+const dates = [
+  { rataDie: -214193, date: { year: -2378, month: 11, day:  4 } },
+  { rataDie:  -61387, date: { year: -1959, month:  3, day: 13 } },
+  { rataDie:   25469, date: { year: -1721, month:  1, day:  2 } },
+  { rataDie:   49217, date: { year: -1656, month:  1, day: 10 } },
+  { rataDie:  171307, date: { year: -1322, month:  4, day: 18 } },
+  { rataDie:  210155, date: { year: -1216, month:  9, day:  1 } },
+  { rataDie:  253427, date: { year: -1097, month:  2, day: 19 } },
+  { rataDie:  369740, date: { year:  -779, month:  8, day:  4 } },
+  { rataDie:  400085, date: { year:  -696, month:  9, day:  5 } },
+  { rataDie:  434355, date: { year:  -602, month:  7, day:  1 } },
+  { rataDie:  452605, date: { year:  -552, month:  6, day: 20 } },
+  { rataDie:  470160, date: { year:  -504, month:  7, day: 13 } },
+  { rataDie:  473837, date: { year:  -494, month:  8, day:  8 } },
+  { rataDie:  507850, date: { year:  -401, month:  9, day: 23 } },
+  { rataDie:  524156, date: { year:  -356, month:  5, day: 13 } },
+  { rataDie:  544676, date: { year:  -300, month:  7, day: 19 } },
+  { rataDie:  567118, date: { year:  -239, month:  0, day:  1 } },
+  { rataDie:  569477, date: { year:  -232, month:  6, day: 14 } },
+  { rataDie:  601716, date: { year:  -144, month:  9, day: 22 } },
+  { rataDie:  613424, date: { year:  -112, month: 10, day: 12 } },
+  { rataDie:  626596, date: { year:   -76, month: 11, day:  6 } },
+  { rataDie:  645554, date: { year:   -24, month: 10, day:  1 } },
+  { rataDie:  664224, date: { year:    27, month: 11, day: 14 } },
+  { rataDie:  671401, date: { year:    47, month:  7, day:  6 } },
+  { rataDie:  694799, date: { year:   111, month:  7, day: 29 } },
+  { rataDie:  704424, date: { year:   137, month: 12, day:  7 } },
+  { rataDie:  708842, date: { year:   150, month:  1, day:  7 } },
+  { rataDie:  709409, date: { year:   151, month:  7, day: 29 } },
+  { rataDie:  709580, date: { year:   152, month:  1, day: 15 } },
+  { rataDie:  727274, date: { year:   200, month:  6, day: 27 } },
+  { rataDie:  728714, date: { year:   204, month:  6, day:  7 } },
+  { rataDie:  744313, date: { year:   247, month:  2, day: 20 } },
+  { rataDie:  764652, date: { year:   302, month: 11, day:  1 } },
 ];
 
 describe ('French Arithmetic calendar spec', () => {
-  let date;
-  let expected;
-  let actual;
-  let julian;
-
   it ('should convert a French Arithmetic date to Julian day', () => {
-    data3.forEach (dt => {
-      date   = dt.frenchArithmetic;
-      julian = dt.rataDie + J0000;
-      actual = cal.toJdn (date.year, date.month, date.day);
-      expect (julian).toBe (actual);
+    dates.forEach (({ rataDie, date }) => {
+      const jdn    = rataDie + J0000;
+      const actual = cal.toJdn (date.year, date.month, date.day);
+
+      expect (actual).toBe (jdn);
     });
   });
 
   it ('should convert a Julian day to a French Arithmetic date', () => {
-    data3.forEach (dt => {
-      julian   = dt.rataDie + J0000;
-      date     = dt.frenchArithmetic;
-      expected = { jdn: julian, year: date.year, month: date.month, day: date.day, yearLeap: cal.isLeapYear(date.year) };
-      actual   = cal.fromJdn (julian);
+    dates.forEach (({ rataDie, date }) => {
+      const jdn      = rataDie + J0000;
+      const yearLeap = cal.isLeapYear(date.year);
+      const expected = { jdn, ...date, yearLeap };
+      const actual   = cal.fromJdn (jdn);
 
       expect (expected).toEqual (actual);
-      // expect (expected.year).toBe (actual.year);
-      // expect (expected.month).toBe (actual.month);
-      // expect (expected.day).toBe (actual.day);
+      expect (expected.year).toBe (actual.getYear());
+      expect (expected.month).toBe (actual.getMonth());
+      expect (expected.day).toBe (actual.getDay());
     });
   });
 
