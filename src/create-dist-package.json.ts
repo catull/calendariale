0b06b270b2eeb265c90c34036bbed5fe868f8689
Carlo Dapor <catull@gmail.com>
2018-12-14 16:22:28 +0100

    Preparation for publishing.

---
+++ src/create-dist-package.json.ts
@@ -0,0 +1,18 @@
+const version = process.argv[2];
+
+const content = {
+  name: 'calendariale',
+  version,
+  author: 'catull@gmail.com',
+  repository: 'catull/calendariale',
+  license: 'BSD-2-Clause',
+  description: 'Calendrical calculations library in TypeScript',
+  main: 'index.js',
+  types: 'index.d.ts',
+  'umd:main': 'index.umd.js',
+  module: 'index.mjs'
+};
+
+const captains = console;
+
+captains.log (JSON.stringify (content, undefined, '  '));
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export * from './Location';
+export * from './Const';
 export * from './Astro';
 export * from './calendar/core';
+export * from './calendar';
