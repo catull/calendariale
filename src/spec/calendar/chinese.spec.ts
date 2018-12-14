50738cab36f4d4e8dd23de633f60db2b081e4e8e
Carlo Dapor <catull@gmail.com>
2018-12-14 23:55:51 +0100

    Cleaned up Chinese calendar spec.

--- src/spec/calendar/chinese.spec.ts
+++ src/spec/calendar/chinese.spec.ts
@@ -2,64 +2,56 @@ import { INVALID_DAY, INVALID_LEAP_MONTH, INVALID_MONTH, INVALID_YEAR, J0000 } f
 
 import { ChineseCalendar as cal } from '../../calendar/ChineseCalendar';
 
-const data4 = [
-  { 'rataDie': -214193, 'chinese': { 'cycle': 35, 'year': 11, 'month':  6, 'monthLeap': false, 'day': 12 } },
-  { 'rataDie':  -61387, 'chinese': { 'cycle': 42, 'year':  9, 'month': 10, 'monthLeap': false, 'day': 27 } },
-  { 'rataDie':   25469, 'chinese': { 'cycle': 46, 'year':  7, 'month':  8, 'monthLeap': false, 'day':  4 } },
-  { 'rataDie':   49217, 'chinese': { 'cycle': 47, 'year': 12, 'month':  8, 'monthLeap': false, 'day':  9 } },
-  { 'rataDie':  171307, 'chinese': { 'cycle': 52, 'year': 46, 'month': 11, 'monthLeap': false, 'day': 20 } },
-  { 'rataDie':  210155, 'chinese': { 'cycle': 54, 'year': 33, 'month':  4, 'monthLeap': false, 'day':  5 } },
-  { 'rataDie':  253427, 'chinese': { 'cycle': 56, 'year': 31, 'month': 10, 'monthLeap': false, 'day': 15 } },
-  { 'rataDie':  369740, 'chinese': { 'cycle': 61, 'year': 50, 'month':  3, 'monthLeap': false, 'day':  7 } },
-  { 'rataDie':  400085, 'chinese': { 'cycle': 63, 'year': 13, 'month':  4, 'monthLeap': false, 'day': 24 } },
-  { 'rataDie':  434355, 'chinese': { 'cycle': 64, 'year': 47, 'month':  2, 'monthLeap': false, 'day':  9 } },
-  { 'rataDie':  452605, 'chinese': { 'cycle': 65, 'year': 37, 'month':  2, 'monthLeap': false, 'day':  9 } },
-  { 'rataDie':  470160, 'chinese': { 'cycle': 66, 'year': 25, 'month':  2, 'monthLeap': false, 'day': 23 } },
-  { 'rataDie':  473837, 'chinese': { 'cycle': 66, 'year': 35, 'month':  3, 'monthLeap': false, 'day':  9 } },
-  { 'rataDie':  507850, 'chinese': { 'cycle': 68, 'year':  8, 'month':  5, 'monthLeap': false, 'day':  2 } },
-  { 'rataDie':  524156, 'chinese': { 'cycle': 68, 'year': 53, 'month':  1, 'monthLeap': false, 'day':  8 } },
-  { 'rataDie':  544676, 'chinese': { 'cycle': 69, 'year': 49, 'month':  3, 'monthLeap': false, 'day':  4 } },
-  { 'rataDie':  567118, 'chinese': { 'cycle': 70, 'year': 50, 'month':  8, 'monthLeap': false, 'day':  2 } },
-  { 'rataDie':  569477, 'chinese': { 'cycle': 70, 'year': 57, 'month':  1, 'monthLeap': false, 'day': 29 } },
-  { 'rataDie':  601716, 'chinese': { 'cycle': 72, 'year': 25, 'month':  4, 'monthLeap': true,  'day': 20 } },
-  { 'rataDie':  613424, 'chinese': { 'cycle': 72, 'year': 57, 'month':  6, 'monthLeap': false, 'day':  5 } },
-  { 'rataDie':  626596, 'chinese': { 'cycle': 73, 'year': 33, 'month':  6, 'monthLeap': false, 'day':  6 } },
-  { 'rataDie':  645554, 'chinese': { 'cycle': 74, 'year': 25, 'month':  5, 'monthLeap': false, 'day':  5 } },
-  { 'rataDie':  664224, 'chinese': { 'cycle': 75, 'year': 16, 'month':  6, 'monthLeap': false, 'day': 12 } },
-  { 'rataDie':  671401, 'chinese': { 'cycle': 75, 'year': 36, 'month':  2, 'monthLeap': false, 'day': 13 } },
-  { 'rataDie':  694799, 'chinese': { 'cycle': 76, 'year': 40, 'month':  3, 'monthLeap': false, 'day': 22 } },
-  { 'rataDie':  704424, 'chinese': { 'cycle': 77, 'year':  6, 'month':  7, 'monthLeap': false, 'day': 21 } },
-  { 'rataDie':  708842, 'chinese': { 'cycle': 77, 'year': 18, 'month':  8, 'monthLeap': false, 'day':  9 } },
-  { 'rataDie':  709409, 'chinese': { 'cycle': 77, 'year': 20, 'month':  3, 'monthLeap': false, 'day': 15 } },
-  { 'rataDie':  709580, 'chinese': { 'cycle': 77, 'year': 20, 'month':  9, 'monthLeap': false, 'day':  9 } },
-  { 'rataDie':  727274, 'chinese': { 'cycle': 78, 'year':  9, 'month':  2, 'monthLeap': false, 'day': 14 } },
-  { 'rataDie':  728714, 'chinese': { 'cycle': 78, 'year': 13, 'month':  1, 'monthLeap': false, 'day':  7 } },
-  { 'rataDie':  744313, 'chinese': { 'cycle': 78, 'year': 55, 'month': 10, 'monthLeap': false, 'day': 14 } },
-  { 'rataDie':  764652, 'chinese': { 'cycle': 79, 'year': 51, 'month':  6, 'monthLeap': false, 'day':  7 } }
+const dates = [
+  { rataDie: -214193, date: { cycle: 35, year: 11, month:  6, monthLeap: false, day: 12 } },
+  { rataDie:  -61387, date: { cycle: 42, year:  9, month: 10, monthLeap: false, day: 27 } },
+  { rataDie:   25469, date: { cycle: 46, year:  7, month:  8, monthLeap: false, day:  4 } },
+  { rataDie:   49217, date: { cycle: 47, year: 12, month:  8, monthLeap: false, day:  9 } },
+  { rataDie:  171307, date: { cycle: 52, year: 46, month: 11, monthLeap: false, day: 20 } },
+  { rataDie:  210155, date: { cycle: 54, year: 33, month:  4, monthLeap: false, day:  5 } },
+  { rataDie:  253427, date: { cycle: 56, year: 31, month: 10, monthLeap: false, day: 15 } },
+  { rataDie:  369740, date: { cycle: 61, year: 50, month:  3, monthLeap: false, day:  7 } },
+  { rataDie:  400085, date: { cycle: 63, year: 13, month:  4, monthLeap: false, day: 24 } },
+  { rataDie:  434355, date: { cycle: 64, year: 47, month:  2, monthLeap: false, day:  9 } },
+  { rataDie:  452605, date: { cycle: 65, year: 37, month:  2, monthLeap: false, day:  9 } },
+  { rataDie:  470160, date: { cycle: 66, year: 25, month:  2, monthLeap: false, day: 23 } },
+  { rataDie:  473837, date: { cycle: 66, year: 35, month:  3, monthLeap: false, day:  9 } },
+  { rataDie:  507850, date: { cycle: 68, year:  8, month:  5, monthLeap: false, day:  2 } },
+  { rataDie:  524156, date: { cycle: 68, year: 53, month:  1, monthLeap: false, day:  8 } },
+  { rataDie:  544676, date: { cycle: 69, year: 49, month:  3, monthLeap: false, day:  4 } },
+  { rataDie:  567118, date: { cycle: 70, year: 50, month:  8, monthLeap: false, day:  2 } },
+  { rataDie:  569477, date: { cycle: 70, year: 57, month:  1, monthLeap: false, day: 29 } },
+  { rataDie:  601716, date: { cycle: 72, year: 25, month:  4, monthLeap: true,  day: 20 } },
+  { rataDie:  613424, date: { cycle: 72, year: 57, month:  6, monthLeap: false, day:  5 } },
+  { rataDie:  626596, date: { cycle: 73, year: 33, month:  6, monthLeap: false, day:  6 } },
+  { rataDie:  645554, date: { cycle: 74, year: 25, month:  5, monthLeap: false, day:  5 } },
+  { rataDie:  664224, date: { cycle: 75, year: 16, month:  6, monthLeap: false, day: 12 } },
+  { rataDie:  671401, date: { cycle: 75, year: 36, month:  2, monthLeap: false, day: 13 } },
+  { rataDie:  694799, date: { cycle: 76, year: 40, month:  3, monthLeap: false, day: 22 } },
+  { rataDie:  704424, date: { cycle: 77, year:  6, month:  7, monthLeap: false, day: 21 } },
+  { rataDie:  708842, date: { cycle: 77, year: 18, month:  8, monthLeap: false, day:  9 } },
+  { rataDie:  709409, date: { cycle: 77, year: 20, month:  3, monthLeap: false, day: 15 } },
+  { rataDie:  709580, date: { cycle: 77, year: 20, month:  9, monthLeap: false, day:  9 } },
+  { rataDie:  727274, date: { cycle: 78, year:  9, month:  2, monthLeap: false, day: 14 } },
+  { rataDie:  728714, date: { cycle: 78, year: 13, month:  1, monthLeap: false, day:  7 } },
+  { rataDie:  744313, date: { cycle: 78, year: 55, month: 10, monthLeap: false, day: 14 } },
+  { rataDie:  764652, date: { cycle: 79, year: 51, month:  6, monthLeap: false, day:  7 } }
 ];
 
 describe ('Chinese calendar spec', () => {
-  let date;
-  let  expected;
-  let  actual;
-  let  julian;
-
   it ('should convert a Chinese date to Julian day', () => {
-    data4.forEach (dt => {
-      date   = dt.chinese;
-      julian = dt.rataDie + J0000;
-      actual = cal.toJdn (date.cycle, date.year, date.month, date.monthLeap, date.day);
-      expect (julian).toBe (actual);
+    dates.forEach (({ rataDie, date }) => {
+      const jdn    = rataDie + J0000;
+      const actual = cal.toJdn (date.cycle, date.year, date.month, date.monthLeap, date.day);
+      expect (jdn).toBe (actual);
     });
   });
 
   it ('should convert a Julian day to a Chinese date', () => {
-    data4.forEach (dt => {
-      julian   = dt.rataDie + J0000;
-      date     = dt.chinese;
-      // expected = { jdn: julian, cycle: date.cycle, year: date.year, month: date.month, monthLeap: date.monthLeap, day: date.day };
-      expected = { jdn: julian, ...date };
-      actual   = cal.fromJdn (julian);
+    dates.forEach (({ rataDie, date }) => {
+      const jdn      = rataDie + J0000;
+      const expected = { jdn, ...date };
+      const actual   = cal.fromJdn (jdn);
 
       expect (expected).toEqual (actual);
       expect (expected.day).toBe (actual.getDay());
