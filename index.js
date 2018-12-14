c392ebe3b0889b6e2186f03affab702bb88fdb33
Carlo Dapor <catull@gmail.com>
2018-12-14 16:22:28 +0100

    Preparation for publishing.

--- index.js
+++ index.js
@@ -1,12 +1,10 @@
-"use strict";
-
-function __export (m) {
-  for (var p in m) {
-    if (!exports.hasOwnProperty (p)) {
-      exports[p] = m[p];
-    }
-  }
+'use strict';
+function __export(m) {
+  for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
-
-__export (require ('./lib/index'));
-
+Object.defineProperty(exports, '__esModule', { value: true });
+__export(require('./dist/Location'));
+__export(require('./dist/Astro'));
+__export(require('./dist/calendar/core'));
+__export(require('./dist/calendar'));
+//# sourceMappingURL=dist/index.js.map
