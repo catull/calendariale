62139d3bd618f5496b21895baf6edbc61d240df8
Carlo Dapor <catull@gmail.com>
2018-12-14 22:56:10 +0100

    Renamed all classes *CalendarDate -> *Date

--- src/spec/calendar/core/year-calendar.spec.ts
+++ src/spec/calendar/core/year-calendar.spec.ts
@@ -1,11 +1,11 @@
-import { YearCalendarDate } from '../../../index';
+import { YearDate } from '../../../index';
 
 describe ('Year calendar spec', () => {
-  it ('should determine a year of an instance of YearCalendarDate', () => {
-    let cal: YearCalendarDate = new YearCalendarDate(-1, 2000);
+  it ('should determine a year of an instance of YearDate', () => {
+    let cal: YearDate = new YearDate(-1, 2000);
     expect (cal.getYear()).toBe(2000);
 
-    cal = new YearCalendarDate(-1, 1900);
+    cal = new YearDate(-1, 1900);
     expect (cal.getYear()).toBe(1900);
   });
 });
