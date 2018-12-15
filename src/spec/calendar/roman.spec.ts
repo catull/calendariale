92f60cbb11d84e35d3fb13494526f5ef4ce48c7d
Carlo Dapor <catull@gmail.com>
2018-12-15 01:47:11 +0100

    Cleaned up Roman calendar spec.

--- src/spec/calendar/roman.spec.ts
+++ src/spec/calendar/roman.spec.ts
@@ -1,62 +1,55 @@
 import { INVALID_COUNT, INVALID_LEAP_DAY, INVALID_MONTH, RomanEvent } from '../../Const';
 import { RomanCalendar as cal } from '../../calendar/RomanCalendar';
 
-const data1 = [
-  { 'julianDay': 1507231.5, 'roman': { 'year': -587, 'month':  8, 'event':  RomanEvent.KALENDS, 'count':  3, 'leap': false } },
-  { 'julianDay': 1660037.5, 'roman': { 'year': -169, 'month': 12, 'event':  RomanEvent.IDES,    'count':  6, 'leap': false } },
-  { 'julianDay': 1746893.5, 'roman': { 'year':   70, 'month': 10, 'event':  RomanEvent.KALENDS, 'count':  6, 'leap': false } },
-  { 'julianDay': 1770641.5, 'roman': { 'year':  135, 'month': 10, 'event':  RomanEvent.NONES,   'count':  5, 'leap': false } },
-  { 'julianDay': 1892731.5, 'roman': { 'year':  470, 'month':  1, 'event':  RomanEvent.IDES,    'count':  7, 'leap': false } },
-  { 'julianDay': 1931579.5, 'roman': { 'year':  576, 'month':  6, 'event':  RomanEvent.KALENDS, 'count': 15, 'leap': false } },
-  { 'julianDay': 1974851.5, 'roman': { 'year':  694, 'month': 11, 'event':  RomanEvent.IDES,    'count':  7, 'leap': false } },
-  { 'julianDay': 2091164.5, 'roman': { 'year': 1013, 'month':  5, 'event':  RomanEvent.KALENDS, 'count': 13, 'leap': false } },
-  { 'julianDay': 2121509.5, 'roman': { 'year': 1096, 'month':  6, 'event':  RomanEvent.KALENDS, 'count': 15, 'leap': false } },
-  { 'julianDay': 2155779.5, 'roman': { 'year': 1190, 'month':  4, 'event':  RomanEvent.KALENDS, 'count': 17, 'leap': false } },
-  { 'julianDay': 2174029.5, 'roman': { 'year': 1240, 'month':  3, 'event':  RomanEvent.NONES,   'count':  5, 'leap': false } },
-  { 'julianDay': 2191584.5, 'roman': { 'year': 1288, 'month':  4, 'event':  RomanEvent.KALENDS, 'count':  7, 'leap': false } },
-  { 'julianDay': 2195261.5, 'roman': { 'year': 1298, 'month':  5, 'event':  RomanEvent.KALENDS, 'count': 12, 'leap': false } },
-  { 'julianDay': 2229274.5, 'roman': { 'year': 1391, 'month':  6, 'event':  RomanEvent.NONES,   'count':  2, 'leap': false } },
-  { 'julianDay': 2245580.5, 'roman': { 'year': 1436, 'month':  2, 'event':  RomanEvent.KALENDS, 'count':  8, 'leap': false } },
-  { 'julianDay': 2266100.5, 'roman': { 'year': 1492, 'month':  4, 'event':  RomanEvent.KALENDS, 'count':  2, 'leap': false } },
-  { 'julianDay': 2288542.5, 'roman': { 'year': 1553, 'month':  9, 'event':  RomanEvent.IDES,    'count':  5, 'leap': false } },
-  { 'julianDay': 2290901.5, 'roman': { 'year': 1560, 'month':  3, 'event':  RomanEvent.KALENDS, 'count':  6, 'leap': false } },
-  { 'julianDay': 2323140.5, 'roman': { 'year': 1648, 'month':  6, 'event':  RomanEvent.KALENDS, 'count':  2, 'leap': false } },
-  { 'julianDay': 2334848.5, 'roman': { 'year': 1680, 'month':  7, 'event':  RomanEvent.KALENDS, 'count': 12, 'leap': false } },
-  { 'julianDay': 2348020.5, 'roman': { 'year': 1716, 'month':  7, 'event':  RomanEvent.IDES,    'count':  3, 'leap': false } },
-  { 'julianDay': 2366978.5, 'roman': { 'year': 1768, 'month':  6, 'event':  RomanEvent.IDES,    'count':  6, 'leap': false } },
-  { 'julianDay': 2385648.5, 'roman': { 'year': 1819, 'month':  8, 'event':  RomanEvent.KALENDS, 'count': 12, 'leap': false } },
-  { 'julianDay': 2392825.5, 'roman': { 'year': 1839, 'month':  3, 'event':  RomanEvent.IDES,    'count':  1, 'leap': false } },
-  { 'julianDay': 2416223.5, 'roman': { 'year': 1903, 'month':  4, 'event':  RomanEvent.IDES,    'count':  8, 'leap': false } },
-  { 'julianDay': 2425848.5, 'roman': { 'year': 1929, 'month':  8, 'event':  RomanEvent.IDES,    'count':  2, 'leap': false } },
-  { 'julianDay': 2430266.5, 'roman': { 'year': 1941, 'month': 10, 'event':  RomanEvent.KALENDS, 'count': 16, 'leap': false } },
-  { 'julianDay': 2430833.5, 'roman': { 'year': 1943, 'month':  4, 'event':  RomanEvent.IDES,    'count':  8, 'leap': false } },
-  { 'julianDay': 2431004.5, 'roman': { 'year': 1943, 'month': 10, 'event':  RomanEvent.KALENDS, 'count':  8, 'leap': false } },
-  { 'julianDay': 2448698.5, 'roman': { 'year': 1992, 'month':  3, 'event':  RomanEvent.NONES,   'count':  4, 'leap': false } },
-  { 'julianDay': 2450138.5, 'roman': { 'year': 1996, 'month':  2, 'event':  RomanEvent.IDES,    'count':  2, 'leap': false } },
-  { 'julianDay': 2465737.5, 'roman': { 'year': 2038, 'month': 11, 'event':  RomanEvent.KALENDS, 'count':  5, 'leap': false } },
-  { 'julianDay': 2486076.5, 'roman': { 'year': 2094, 'month':  7, 'event':  RomanEvent.NONES,   'count':  3, 'leap': false } }
+const dates = [
+  { jdn: 1507231.5, date: { year: -587, month:  8, event:  RomanEvent.KALENDS, count:  3, leap: false } },
+  { jdn: 1660037.5, date: { year: -169, month: 12, event:  RomanEvent.IDES,    count:  6, leap: false } },
+  { jdn: 1746893.5, date: { year:   70, month: 10, event:  RomanEvent.KALENDS, count:  6, leap: false } },
+  { jdn: 1770641.5, date: { year:  135, month: 10, event:  RomanEvent.NONES,   count:  5, leap: false } },
+  { jdn: 1892731.5, date: { year:  470, month:  1, event:  RomanEvent.IDES,    count:  7, leap: false } },
+  { jdn: 1931579.5, date: { year:  576, month:  6, event:  RomanEvent.KALENDS, count: 15, leap: false } },
+  { jdn: 1974851.5, date: { year:  694, month: 11, event:  RomanEvent.IDES,    count:  7, leap: false } },
+  { jdn: 2091164.5, date: { year: 1013, month:  5, event:  RomanEvent.KALENDS, count: 13, leap: false } },
+  { jdn: 2121509.5, date: { year: 1096, month:  6, event:  RomanEvent.KALENDS, count: 15, leap: false } },
+  { jdn: 2155779.5, date: { year: 1190, month:  4, event:  RomanEvent.KALENDS, count: 17, leap: false } },
+  { jdn: 2174029.5, date: { year: 1240, month:  3, event:  RomanEvent.NONES,   count:  5, leap: false } },
+  { jdn: 2191584.5, date: { year: 1288, month:  4, event:  RomanEvent.KALENDS, count:  7, leap: false } },
+  { jdn: 2195261.5, date: { year: 1298, month:  5, event:  RomanEvent.KALENDS, count: 12, leap: false } },
+  { jdn: 2229274.5, date: { year: 1391, month:  6, event:  RomanEvent.NONES,   count:  2, leap: false } },
+  { jdn: 2245580.5, date: { year: 1436, month:  2, event:  RomanEvent.KALENDS, count:  8, leap: false } },
+  { jdn: 2266100.5, date: { year: 1492, month:  4, event:  RomanEvent.KALENDS, count:  2, leap: false } },
+  { jdn: 2288542.5, date: { year: 1553, month:  9, event:  RomanEvent.IDES,    count:  5, leap: false } },
+  { jdn: 2290901.5, date: { year: 1560, month:  3, event:  RomanEvent.KALENDS, count:  6, leap: false } },
+  { jdn: 2323140.5, date: { year: 1648, month:  6, event:  RomanEvent.KALENDS, count:  2, leap: false } },
+  { jdn: 2334848.5, date: { year: 1680, month:  7, event:  RomanEvent.KALENDS, count: 12, leap: false } },
+  { jdn: 2348020.5, date: { year: 1716, month:  7, event:  RomanEvent.IDES,    count:  3, leap: false } },
+  { jdn: 2366978.5, date: { year: 1768, month:  6, event:  RomanEvent.IDES,    count:  6, leap: false } },
+  { jdn: 2385648.5, date: { year: 1819, month:  8, event:  RomanEvent.KALENDS, count: 12, leap: false } },
+  { jdn: 2392825.5, date: { year: 1839, month:  3, event:  RomanEvent.IDES,    count:  1, leap: false } },
+  { jdn: 2416223.5, date: { year: 1903, month:  4, event:  RomanEvent.IDES,    count:  8, leap: false } },
+  { jdn: 2425848.5, date: { year: 1929, month:  8, event:  RomanEvent.IDES,    count:  2, leap: false } },
+  { jdn: 2430266.5, date: { year: 1941, month: 10, event:  RomanEvent.KALENDS, count: 16, leap: false } },
+  { jdn: 2430833.5, date: { year: 1943, month:  4, event:  RomanEvent.IDES,    count:  8, leap: false } },
+  { jdn: 2431004.5, date: { year: 1943, month: 10, event:  RomanEvent.KALENDS, count:  8, leap: false } },
+  { jdn: 2448698.5, date: { year: 1992, month:  3, event:  RomanEvent.NONES,   count:  4, leap: false } },
+  { jdn: 2450138.5, date: { year: 1996, month:  2, event:  RomanEvent.IDES,    count:  2, leap: false } },
+  { jdn: 2465737.5, date: { year: 2038, month: 11, event:  RomanEvent.KALENDS, count:  5, leap: false } },
+  { jdn: 2486076.5, date: { year: 2094, month:  7, event:  RomanEvent.NONES,   count:  3, leap: false } },
 ];
 
-
 describe ('Roman calendar spec', () => {
-  let date;
-  let expected;
-  let actual;
-
   it ('should convert a Roman date to Julian day', () => {
-    data1.forEach (dt => {
-      date   = dt.roman;
-      actual = cal.toJdn (date.year, date.month, date.event, date.count, date.leap);
+    dates.forEach (({ jdn, date }) => {
+      const actual = cal.toJdn (date.year, date.month, date.event, date.count, date.leap);
 
-      expect (dt.julianDay).toBe (actual);
+      expect (actual).toBe (jdn);
     });
   });
 
   it ('should convert a Julian day to a Roman date', () => {
-    data1.forEach (dt => {
-      date     = dt.roman;
-      expected = { 'jdn': dt.julianDay, 'day': -1, 'year': date.year, 'month': date.month, 'event': date.event, 'count': date.count, 'leap': date.leap };
-      actual   = cal.fromJdn (dt.julianDay);
+    dates.forEach (({ jdn, date }) => {
+      const expected = { jdn, day: -1, ...date };
+      const actual   = cal.fromJdn (jdn);
 
       expect (expected).toEqual (actual);
       expect (expected.year).toBe (actual.getYear());
