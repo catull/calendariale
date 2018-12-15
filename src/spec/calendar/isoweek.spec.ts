17c9b1f4a8b6ab7d3dc5e3ca97c3bde3e59675f5
Carlo Dapor <catull@gmail.com>
2018-12-15 01:18:03 +0100

    Cleaned up ISO Week calendar spec.

--- src/spec/calendar/isoweek.spec.ts
+++ src/spec/calendar/isoweek.spec.ts
@@ -1,62 +1,55 @@
 import { INVALID_DAY, INVALID_WEEK } from '../../Const';
 import { IsoWeekCalendar as cal } from '../../calendar/IsoWeekCalendar';
 
-const data1 = [
-  { 'julianDay': 1507231.5, 'iso': { 'year': -586, 'week': 29, 'day': 7 } },
-  { 'julianDay': 1660037.5, 'iso': { 'year': -168, 'week': 49, 'day': 3 } },
-  { 'julianDay': 1746893.5, 'iso': { 'year':   70, 'week': 39, 'day': 3 } },
-  { 'julianDay': 1770641.5, 'iso': { 'year':  135, 'week': 39, 'day': 7 } },
-  { 'julianDay': 1892731.5, 'iso': { 'year':  470, 'week':  2, 'day': 3 } },
-  { 'julianDay': 1931579.5, 'iso': { 'year':  576, 'week': 21, 'day': 1 } },
-  { 'julianDay': 1974851.5, 'iso': { 'year':  694, 'week': 45, 'day': 6 } },
-  { 'julianDay': 2091164.5, 'iso': { 'year': 1013, 'week': 16, 'day': 7 } },
-  { 'julianDay': 2121509.5, 'iso': { 'year': 1096, 'week': 21, 'day': 7 } },
-  { 'julianDay': 2155779.5, 'iso': { 'year': 1190, 'week': 12, 'day': 5 } },
-  { 'julianDay': 2174029.5, 'iso': { 'year': 1240, 'week': 10, 'day': 6 } },
-  { 'julianDay': 2191584.5, 'iso': { 'year': 1288, 'week': 14, 'day': 5 } },
-  { 'julianDay': 2195261.5, 'iso': { 'year': 1298, 'week': 17, 'day': 7 } },
-  { 'julianDay': 2229274.5, 'iso': { 'year': 1391, 'week': 23, 'day': 7 } },
-  { 'julianDay': 2245580.5, 'iso': { 'year': 1436, 'week':  5, 'day': 3 } },
-  { 'julianDay': 2266100.5, 'iso': { 'year': 1492, 'week': 14, 'day': 6 } },
-  { 'julianDay': 2288542.5, 'iso': { 'year': 1553, 'week': 38, 'day': 6 } },
-  { 'julianDay': 2290901.5, 'iso': { 'year': 1560, 'week':  9, 'day': 6 } },
-  { 'julianDay': 2323140.5, 'iso': { 'year': 1648, 'week': 24, 'day': 3 } },
-  { 'julianDay': 2334848.5, 'iso': { 'year': 1680, 'week': 26, 'day': 7 } },
-  { 'julianDay': 2348020.5, 'iso': { 'year': 1716, 'week': 30, 'day': 5 } },
-  { 'julianDay': 2366978.5, 'iso': { 'year': 1768, 'week': 24, 'day': 7 } },
-  { 'julianDay': 2385648.5, 'iso': { 'year': 1819, 'week': 31, 'day': 1 } },
-  { 'julianDay': 2392825.5, 'iso': { 'year': 1839, 'week': 13, 'day': 3 } },
-  { 'julianDay': 2416223.5, 'iso': { 'year': 1903, 'week': 16, 'day': 7 } },
-  { 'julianDay': 2425848.5, 'iso': { 'year': 1929, 'week': 34, 'day': 7 } },
-  { 'julianDay': 2430266.5, 'iso': { 'year': 1941, 'week': 40, 'day': 1 } },
-  { 'julianDay': 2430833.5, 'iso': { 'year': 1943, 'week': 16, 'day': 1 } },
-  { 'julianDay': 2431004.5, 'iso': { 'year': 1943, 'week': 40, 'day': 4 } },
-  { 'julianDay': 2448698.5, 'iso': { 'year': 1992, 'week': 12, 'day': 2 } },
-  { 'julianDay': 2450138.5, 'iso': { 'year': 1996, 'week':  8, 'day': 7 } },
-  { 'julianDay': 2465737.5, 'iso': { 'year': 2038, 'week': 45, 'day': 3 } },
-  { 'julianDay': 2486076.5, 'iso': { 'year': 2094, 'week': 28, 'day': 7 } }
+const dates = [
+  { jdn: 1507231.5, date: { year: -586, week: 29, day: 7 } },
+  { jdn: 1660037.5, date: { year: -168, week: 49, day: 3 } },
+  { jdn: 1746893.5, date: { year:   70, week: 39, day: 3 } },
+  { jdn: 1770641.5, date: { year:  135, week: 39, day: 7 } },
+  { jdn: 1892731.5, date: { year:  470, week:  2, day: 3 } },
+  { jdn: 1931579.5, date: { year:  576, week: 21, day: 1 } },
+  { jdn: 1974851.5, date: { year:  694, week: 45, day: 6 } },
+  { jdn: 2091164.5, date: { year: 1013, week: 16, day: 7 } },
+  { jdn: 2121509.5, date: { year: 1096, week: 21, day: 7 } },
+  { jdn: 2155779.5, date: { year: 1190, week: 12, day: 5 } },
+  { jdn: 2174029.5, date: { year: 1240, week: 10, day: 6 } },
+  { jdn: 2191584.5, date: { year: 1288, week: 14, day: 5 } },
+  { jdn: 2195261.5, date: { year: 1298, week: 17, day: 7 } },
+  { jdn: 2229274.5, date: { year: 1391, week: 23, day: 7 } },
+  { jdn: 2245580.5, date: { year: 1436, week:  5, day: 3 } },
+  { jdn: 2266100.5, date: { year: 1492, week: 14, day: 6 } },
+  { jdn: 2288542.5, date: { year: 1553, week: 38, day: 6 } },
+  { jdn: 2290901.5, date: { year: 1560, week:  9, day: 6 } },
+  { jdn: 2323140.5, date: { year: 1648, week: 24, day: 3 } },
+  { jdn: 2334848.5, date: { year: 1680, week: 26, day: 7 } },
+  { jdn: 2348020.5, date: { year: 1716, week: 30, day: 5 } },
+  { jdn: 2366978.5, date: { year: 1768, week: 24, day: 7 } },
+  { jdn: 2385648.5, date: { year: 1819, week: 31, day: 1 } },
+  { jdn: 2392825.5, date: { year: 1839, week: 13, day: 3 } },
+  { jdn: 2416223.5, date: { year: 1903, week: 16, day: 7 } },
+  { jdn: 2425848.5, date: { year: 1929, week: 34, day: 7 } },
+  { jdn: 2430266.5, date: { year: 1941, week: 40, day: 1 } },
+  { jdn: 2430833.5, date: { year: 1943, week: 16, day: 1 } },
+  { jdn: 2431004.5, date: { year: 1943, week: 40, day: 4 } },
+  { jdn: 2448698.5, date: { year: 1992, week: 12, day: 2 } },
+  { jdn: 2450138.5, date: { year: 1996, week:  8, day: 7 } },
+  { jdn: 2465737.5, date: { year: 2038, week: 45, day: 3 } },
+  { jdn: 2486076.5, date: { year: 2094, week: 28, day: 7 } },
 ];
 
 describe ('ISO Week calendar spec', () => {
-  let date;
-  let expected;
-  let actual;
-
   it ('should convert an ISO Week date to Julian day', () => {
-    data1.forEach (dt => {
-      date     = dt.iso;
-      actual   = cal.toJdn (date.year, date.week, date.day);
+    dates.forEach (({ jdn, date }) => {
+      const actual = cal.toJdn (date.year, date.week, date.day);
 
-      expect (dt.julianDay).toBe (actual);
+      expect (actual).toBe (jdn);
     });
   });
 
   it ('should convert a Julian day to an ISO Week date', () => {
-    data1.forEach (dt => {
-      date     = dt.iso;
-      // expected = { 'jdn': dt.julianDay, 'year': date.year, 'week': date.week, 'day': date.day };
-      expected = { 'jdn': dt.julianDay, ...date };
-      actual   = cal.fromJdn (dt.julianDay);
+    dates.forEach (({ jdn, date }) => {
+      const expected = { jdn, ...date };
+      const actual   = cal.fromJdn (jdn);
 
       expect (expected).toEqual (actual);
       expect (expected.year).toBe (actual.getYear());
