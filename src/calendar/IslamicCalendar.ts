89b1f8129efd48f53708cd84b4b06b03697f5008
Carlo Dapor <catull@gmail.com>
2018-12-21 02:51:06 +0100

    Modify tests to use fromRd now: Islamic.

--- src/calendar/IslamicCalendar.ts
+++ src/calendar/IslamicCalendar.ts
@@ -1,10 +1,11 @@
 import { mod } from '../Astro';
 import { INVALID_DAY, INVALID_MONTH, islamic } from '../Const';
 
+import { BaseCalendar } from './BaseCalendar';
 import { IslamicDate } from './IslamicDate';
 import { CalendarDateValidationException } from './core';
 
-export class IslamicCalendar {
+export class IslamicCalendar extends BaseCalendar {
   // Is a given year in the Islamic calendar a leap year?
   public static isLeapYear(year: number): boolean {
     return (year * 11 + 14) % 30 < 11;
