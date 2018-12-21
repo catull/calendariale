e45369931a3ee143c483315236e2570400a077b0
Carlo Dapor <catull@gmail.com>
2018-12-21 02:53:17 +0100

    Modify tests to use fromRd now: Mayan Tzolkin.

--- src/calendar/MayanTzolkinCalendar.ts
+++ src/calendar/MayanTzolkinCalendar.ts
@@ -1,9 +1,10 @@
 import { amod } from '../Astro';
 import { mayan } from '../Const';
 
+import { BaseCalendar } from './BaseCalendar';
 import { MayanTzolkinDate } from './MayanTzolkinDate';
 
-export class MayanTzolkinCalendar {
+export class MayanTzolkinCalendar extends BaseCalendar {
   // Calculate Mayan Tzolkin calendar date from Julian day
   public static fromJdn(jdn: number): MayanTzolkinDate {
     const count: number = Math.floor(jdn - 0.5) + 0.5 - mayan.EPOCH;
