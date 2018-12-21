e2773a61225913f23b482730f9dae57860917f94
Carlo Dapor <catull@gmail.com>
2018-12-21 02:45:45 +0100

    Modify tests to use fromRd now: Ethiopic.

--- src/calendar/EthiopicCalendar.ts
+++ src/calendar/EthiopicCalendar.ts
@@ -1,10 +1,11 @@
 import { mod } from '../Astro';
 import { INVALID_DAY, INVALID_MONTH, ethiopic } from '../Const';
 
+import { BaseCalendar } from './BaseCalendar';
 import { EthiopicDate } from './EthiopicDate';
 import { CalendarDateValidationException } from './core';
 
-export class EthiopicCalendar {
+export class EthiopicCalendar extends BaseCalendar {
   // Determine Julian day number from Ethiopic calendar date
   public static toJdn(year: number, month: number, day: number): number {
     this.validate(year, month, day);
