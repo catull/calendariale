653ee50f5981a5beafd1fce874e3142832ae7dc9
Carlo Dapor <catull@gmail.com>
2018-12-21 02:50:31 +0100

    Modify tests to use fromRd now: Hindu Solar Old.

--- src/calendar/HinduSolarOldCalendar.ts
+++ src/calendar/HinduSolarOldCalendar.ts
@@ -1,28 +1,18 @@
 import { mod } from '../Astro';
 import { ARYA_SOLAR_MONTH, ARYA_SOLAR_YEAR, INVALID_DAY, INVALID_MONTH, hindu } from '../Const';
 
+import { BaseCalendar } from './BaseCalendar';
 import { hinduDayCount } from './HinduAlgorithms';
 import { HinduSolarOldDate } from './HinduSolarOldDate';
 import { CalendarDateValidationException } from './core';
 
-export class HinduSolarOldCalendar {
+export class HinduSolarOldCalendar extends BaseCalendar {
   // Determine Julian day number from Hindu Solar Old calendar date
   public static toJdn(year: number, month: number, day: number): number {
     this.validate(year, month, day);
 
     return Math.ceil(hindu.EPOCH + year * ARYA_SOLAR_YEAR +
       (month - 1) * ARYA_SOLAR_MONTH + day - 0.75) - 0.5;
-  }
-
-  public static validate(year: number, month: number, day: number): void {
-    if (month < 1 || month > 12) {
-      throw new CalendarDateValidationException(INVALID_MONTH);
-    }
-
-    const maxDays: number = (month < 7) ? 31 : 30;
-    if (day < 1 || day > maxDays) {
-      throw new CalendarDateValidationException(INVALID_DAY);
-    }
   }
 
   // Calculate Hindu Solar Old calendar date from Julian day
@@ -33,6 +23,17 @@ export class HinduSolarOldCalendar {
     const day: number = Math.floor(mod(sun, ARYA_SOLAR_MONTH)) + 1;
 
     return new HinduSolarOldDate(jdn, year, month, day);
+  }
+
+  private static validate(year: number, month: number, day: number): void {
+    if (month < 1 || month > 12) {
+      throw new CalendarDateValidationException(INVALID_MONTH);
+    }
+
+    const maxDays: number = (month < 7) ? 31 : 30;
+    if (day < 1 || day > maxDays) {
+      throw new CalendarDateValidationException(INVALID_DAY);
+    }
   }
 
 }
