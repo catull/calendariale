6190bc873f71fae43537b0d12c262a718baf05a8
Carlo Dapor <catull@gmail.com>
2018-12-23 00:04:56 +0100

    New calendar: Icelandic.

---
+++ src/calendar/IcelandicDate.ts
@@ -0,0 +1,23 @@
+import { Season } from '../Const';
+
+import { YearDate } from './core';
+
+export class IcelandicDate extends YearDate {
+
+  constructor(jdn: number, year: number, private season: Season, private week: number, private day: number) {
+    super(jdn, year);
+  }
+
+  public getSeason(): Season {
+    return this.season;
+  }
+
+  public getWeek(): number {
+    return this.week;
+  }
+
+  public getDay(): number {
+    return this.day;
+  }
+
+}
