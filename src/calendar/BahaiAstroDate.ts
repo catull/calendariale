839aa8d7d8ca8ef87b7bab56c4207a0333c12692
Carlo Dapor <catull@gmail.com>
2018-12-25 01:39:18 +0100

    New calendar: Bahai Astro.

---
+++ src/calendar/BahaiAstroDate.ts
@@ -0,0 +1,23 @@
+import { BahaiAstroCalendar } from './BahaiAstroCalendar';
+import { LeapDate } from './core';
+
+export class BahaiAstroDate extends LeapDate {
+
+  constructor(
+    jdn: number,
+    private kullIShay: number,
+    private vahid: number,
+    protected year: number,
+    protected month: number,
+    protected day: number) {
+    super(jdn, year, month, day, BahaiAstroCalendar.isLeapYear(year));
+  }
+
+  public getKullIShay (): number {
+    return this.kullIShay;
+  }
+
+  public getVahid (): number {
+    return this.vahid;
+  }
+}
