87dc302fd1ef3a4cf019811ed730017a054981f3
Carlo Dapor <catull@gmail.com>
2018-12-28 14:38:49 +0100

    New calendar: Islamic (Saudi) Umm al-Qura

---
+++ src/calendar/IslamicUmmAlQuraDate.ts
@@ -0,0 +1,10 @@
+import { IslamicUmmAlQuraCalendar } from './IslamicUmmAlQuraCalendar';
+import { LeapDate } from './core';
+
+export class IslamicUmmAlQuraDate extends LeapDate {
+
+  constructor(jdn: number, year: number, month: number, day: number) {
+    super(jdn, year, month, day, IslamicUmmAlQuraCalendar.isLeapYear(year));
+  }
+
+}
