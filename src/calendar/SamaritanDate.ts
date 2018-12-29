e1d352c588267f3fe14ce822a3301c676d14ad74
Carlo Dapor <catull@gmail.com>
2018-12-29 19:00:36 +0100

    New Calendar: Samaritan

---
+++ src/calendar/SamaritanDate.ts
@@ -0,0 +1,10 @@
+import { SamaritanCalendar } from './SamaritanCalendar';
+import { LeapDate } from './core';
+
+export class SamaritanDate extends LeapDate {
+
+  constructor(jdn: number, year: number, month: number, day: number) {
+    super(jdn, year, month, day, SamaritanCalendar.isLeapYear(year));
+  }
+
+}
