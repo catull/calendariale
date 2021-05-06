c9e18823cd9499aacf422b276139d9facaed21ee
Carlo Dapor <catull@gmail.com>
2021-05-06 15:56:35 +0200

    Added barrion configuration.

--- .eslintrc.spec.js
+++ .eslintrc.spec.js
@@ -3,9 +3,9 @@ module.exports =  {
   plugins: ['@typescript-eslint'],
   extends:  [
     'plugin:@typescript-eslint/recommended',  // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   parserOptions:  {
     ecmaVersion:  2018,  // Allows for the parsing of modern ECMAScript features
