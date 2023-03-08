4d86042b810a6f6eca270485cd72a7f76c576dff
Carlo Dapor <catull@gmail.com>
2023-03-08 16:55:16 +0100

    "Converted class to ESM, adapted import statements, fixed ESlint findings for src/calendar/BabylonianDate.ts"

--- src/calendar/BabylonianDate.ts
+++ src/calendar/BabylonianDate.ts
@@ -1,4 +1,4 @@
-import { LeapDate } from './core';
+import { LeapDate } from './core/index';
 
 export class BabylonianDate extends LeapDate {
   constructor(jdn: number, year: number, month: number, leap: boolean, day: number) {
