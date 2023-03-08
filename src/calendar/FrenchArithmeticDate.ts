584a17e15fbefb07182eb84016dc9c380250676c
Carlo Dapor <catull@gmail.com>
2023-03-08 16:55:17 +0100

    "Converted class to ESM, adapted import statements, fixed ESlint findings for src/calendar/FrenchArithmeticDate.ts"

--- src/calendar/FrenchArithmeticDate.ts
+++ src/calendar/FrenchArithmeticDate.ts
@@ -1,5 +1,5 @@
 import { FrenchArithmeticCalendar } from './FrenchArithmeticCalendar';
-import { LeapDate } from './core';
+import { LeapDate } from './core/index';
 
 export class FrenchArithmeticDate extends LeapDate {
   constructor(jdn: number, year: number, month: number, day: number) {
