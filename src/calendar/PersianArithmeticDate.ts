3ad3ae5d29b312818a8c5c26a86f93e263b1a535
Carlo Dapor <catull@gmail.com>
2023-03-08 16:55:17 +0100

    "Converted class to ESM, adapted import statements, fixed ESlint findings for src/calendar/PersianArithmeticDate.ts"

--- src/calendar/PersianArithmeticDate.ts
+++ src/calendar/PersianArithmeticDate.ts
@@ -1,5 +1,5 @@
 import { PersianArithmeticCalendar } from './PersianArithmeticCalendar';
-import { LeapDate } from './core';
+import { LeapDate } from './core/index';
 
 export class PersianArithmeticDate extends LeapDate {
   constructor(jdn: number, year: number, month: number, day: number) {
