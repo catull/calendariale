036f97cbf9a1400856f17e9ebad332cdb3d8b09e
Carlo Dapor <carlo.dapor@mimacom.com>
2024-04-13 20:24:31 +0200

    Upgraded to ESLint 9.0

--- vitest.config.js
+++ vitest.config.js
@@ -2,35 +2,22 @@ import { mergeConfig } from 'vite';
 import { defineConfig } from 'vitest/config';
 
 export default mergeConfig(
-    defineConfig({
-        test: {
-            globals: true,
-            environment: 'happy-dom',
-            include: [
-                'src/spec/**/*.test.ts',
-            ],
-            coverage: {
-                provider: 'v8',
-                reporter: [
-                    'clover',
-                    'cobertura',
-                    'html',
-                    'json',
-                    'text',
-                ],
-                exclude: [
-                    'src/spec/**',
-                    'src/**/index.ts',
-                ],
-                include: [
-                    'src/**/*.ts',
-                ],
-                reportsDirectory: 'tmp/reports',
-                branches: 99.9,
-                functions: 100,
-                lines: 100,
-                statements: 100,
-            },
-        },
-    }),
+  defineConfig({
+    test: {
+      globals: true,
+      environment: 'happy-dom',
+      include: ['src/spec/**/*.test.ts'],
+      coverage: {
+        provider: 'v8',
+        reporter: ['clover', 'cobertura', 'html', 'json', 'text'],
+        exclude: ['src/spec/**', 'src/**/index.ts'],
+        include: ['src/**/*.ts'],
+        reportsDirectory: 'tmp/reports',
+        branches: 99.9,
+        functions: 100,
+        lines: 100,
+        statements: 100
+      }
+    }
+  })
 );
