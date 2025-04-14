29a155b1147eb9c53ba9733de6d509b98c2bc105
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:10:29 +0200

    Fixed lint findings

--- src/calendar/FrenchArithmeticDate.ts
+++ src/calendar/FrenchArithmeticDate.ts
@@ -1,8 +1,8 @@
-import { FrenchArithmeticCalendar } from './FrenchArithmeticCalendar';
-import { LeapDate } from './core/index';
+import { FrenchArithmeticCalendar } from './FrenchArithmeticCalendar'
+import { LeapDate } from './core/index'
 
 export class FrenchArithmeticDate extends LeapDate {
-  constructor(jdn: number, year: number, month: number, day: number) {
-    super(jdn, year, month, day, FrenchArithmeticCalendar.isLeapYear(year));
+  constructor (jdn: number, year: number, month: number, day: number) {
+    super(jdn, year, month, day, FrenchArithmeticCalendar.isLeapYear(year))
   }
 }
