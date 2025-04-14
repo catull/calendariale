fffe33da5db4b96b4ea28bb15d304da41aa2629b
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:10:31 +0200

    Fixed lint findings

--- src/spec/calendar/hebrew.observational.test.ts
+++ src/spec/calendar/hebrew.observational.test.ts
@@ -41,15 +41,15 @@ const dates = [
 
 describe('hebrew Observational calendar spec', () => {
   it('should convert a Hebrew Observational date to Julian day number (JDN)', () => {
-    dates.forEach(({ jdn, date }) => {
+    for (const { jdn, date } of dates) {
       const actual = cal.toJdn(date.year, date.month, date.day);
 
       expect(actual).toBe(jdn);
-    });
+    };
   });
 
   it('should convert a Julian day number (JDN) to a Hebrew Observational date', () => {
-    dates.forEach(({ jdn, date }) => {
+    for (const { jdn, date } of dates) {
       const actual = cal.fromJdn(jdn);
       const expected = { jdn, ...date };
 
@@ -57,7 +57,7 @@ describe('hebrew Observational calendar spec', () => {
       expect(expected.year).toBe(actual.getYear());
       expect(expected.month).toBe(actual.getMonth());
       expect(expected.day).toBe(actual.getDay());
-    });
+    };
   });
 
   it('should throw validation exceptions', () => {
