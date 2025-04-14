638396d764e1b75cdda0d181b35cf711c232e6f3
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:10:29 +0200

    Fixed lint findings

--- src/calendar/FrenchArithmeticCalendar.ts
+++ src/calendar/FrenchArithmeticCalendar.ts
@@ -1,31 +1,31 @@
-import { mod } from '../Astro';
-import { INVALID_DAY, INVALID_MONTH, french } from '../Const';
+import { mod } from '../Astro'
+import { INVALID_DAY, INVALID_MONTH, french } from '../Const'
 
-import { FrenchArithmeticDate } from './FrenchArithmeticDate';
-import { CalendarDateValidationException } from './core/index';
+import { FrenchArithmeticDate } from './FrenchArithmeticDate'
+import { CalendarDateValidationException } from './core/index'
 
 export class FrenchArithmeticCalendar {
   // Calculate date in the French Arithmetic calendar from Julian day number (JDN).
-  public static fromJdn(jdn: number): FrenchArithmeticDate {
-    const approx: number = Math.floor((jdn - french.EPOCH + 2) / (1460969 / 4000)) + 1;
-    const year: number = jdn < this.toJdn(approx, 1, 1) ? approx - 1 : approx;
-    let month: number = 1 + Math.floor((jdn - this.toJdn(year, 1, 1)) / 30);
+  public static fromJdn (jdn: number): FrenchArithmeticDate {
+    const approx: number = Math.floor((jdn - french.EPOCH + 2) / (1460969 / 4000)) + 1
+    const year: number = jdn < this.toJdn(approx, 1, 1) ? approx - 1 : approx
+    let month: number = 1 + Math.floor((jdn - this.toJdn(year, 1, 1)) / 30)
 
     if (month === 13) {
-      month = 0;
+      month = 0
     }
 
-    const day: number = jdn - this.toJdn(year, month, 1) + 1;
+    const day: number = jdn - this.toJdn(year, month, 1) + 1
 
-    return new FrenchArithmeticDate(jdn, year, month, day);
+    return new FrenchArithmeticDate(jdn, year, month, day)
   }
 
   // Determine Julian day number (JDN) from French Arithmetic calendar date
-  public static toJdn(year: number, month: number, day: number): number {
-    this.validate(year, month, day);
+  public static toJdn (year: number, month: number, day: number): number {
+    this.validate(year, month, day)
 
-    const y1: number = year - 1;
-    const m = month === 0 ? 12 : month - 1;
+    const y1: number = year - 1
+    const m = month === 0 ? 12 : month - 1
 
     return (
       french.EPOCH -
@@ -37,29 +37,29 @@ export class FrenchArithmeticCalendar {
       Math.floor(y1 / 4000) +
       30 * m +
       day
-    );
+    )
   }
 
   // Is the given year a leap year in the French Arithmetic calendar ?
-  public static isLeapYear(year: number): boolean {
-    const m400: number = mod(year, 400);
+  public static isLeapYear (year: number): boolean {
+    const m400: number = mod(year, 400)
 
-    return mod(year, 4) === 0 && m400 !== 100 && m400 !== 200 && m400 !== 300 && mod(year, 4000) !== 0;
+    return mod(year, 4) === 0 && m400 !== 100 && m400 !== 200 && m400 !== 300 && mod(year, 4000) !== 0
   }
 
-  private static validate(year: number, month: number, day: number): void {
+  private static validate (year: number, month: number, day: number): void {
     if (month < 0 || month > 12) {
-      throw new CalendarDateValidationException(INVALID_MONTH);
+      throw new CalendarDateValidationException(INVALID_MONTH)
     }
 
-    const days: number = this.isLeapYear(year) ? 6 : 5;
+    const days: number = this.isLeapYear(year) ? 6 : 5
 
     if (month === 0 && day > days) {
-      throw new CalendarDateValidationException(INVALID_DAY);
+      throw new CalendarDateValidationException(INVALID_DAY)
     }
 
     if (day < 1 || day > 30) {
-      throw new CalendarDateValidationException(INVALID_DAY);
+      throw new CalendarDateValidationException(INVALID_DAY)
     }
   }
 }
