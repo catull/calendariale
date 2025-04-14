5ad82d4c6382d63421f1ca5496a50d73f66832d7
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:10:31 +0200

    Fixed lint findings

--- src/spec/calendar/hindu.solar.modern.test.ts
+++ src/spec/calendar/hindu.solar.modern.test.ts
@@ -41,15 +41,15 @@ const dates = [
 
 describe('hindu Solar Modern calendar spec', () => {
   it('should convert a Hindu Solar Modern date to Julian day number (JDN)', () => {
-    dates.forEach(({ jdn, date }) => {
+    for (const { jdn, date } of dates) {
       const actual = cal.toJdn(date.year, date.month, date.day);
 
       expect(actual).toBe(jdn);
-    });
+    };
   });
 
   it('should convert a Julian day number (JDN) to a Hindu Solar Modern date', () => {
-    dates.forEach(({ jdn, date }) => {
+    for (const { jdn, date } of dates) {
       const actual = cal.fromJdn(jdn);
       const expected = { jdn, ...date };
 
@@ -57,7 +57,7 @@ describe('hindu Solar Modern calendar spec', () => {
       expect(expected.year).toBe(actual.getYear());
       expect(expected.month).toBe(actual.getMonth());
       expect(expected.day).toBe(actual.getDay());
-    });
+    };
   });
 
   it('should throw validation exceptions', () => {
