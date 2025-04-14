56f9a69f95c3e330b8d7f00912277c07bc1a532c
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:10:32 +0200

    Fixed lint findings

--- src/spec/calendar/mayan.tzolkin.test.ts
+++ src/spec/calendar/mayan.tzolkin.test.ts
@@ -40,13 +40,13 @@ const dates = [
 
 describe('mayan Tzolkin calendar spec', () => {
   it('should convert a Julian day number (JDN) to a Mayan Tzolkin', () => {
-    dates.forEach(({ jdn, date }) => {
+    for (const { jdn, date } of dates) {
       const actual = cal.fromJdn(jdn);
       const expected = { jdn, ...date };
 
       expect(expected).toEqual(actual);
       expect(expected.month).toBe(actual.getMonth());
       expect(expected.day).toBe(actual.getDay());
-    });
+    };
   });
 });
