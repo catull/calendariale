aa8ac8713744809a16f2c4f2e6b59df93eecf3ef
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:12:51 +0200

    Fixed lint findings

--- src/calendar/IslamicUmmAlQuraCalendar.ts
+++ src/calendar/IslamicUmmAlQuraCalendar.ts
@@ -30,7 +30,7 @@ export class IslamicUmmAlQuraCalendar {
     return (year * 11 + 14) % 30 < 11
   }
 
-  private static validate (year: number, month: number, day: number): void {
+  private static validate (_year: number, month: number, day: number): void {
     if (month < 1 || month > 12) {
       throw new CalendarDateValidationException(INVALID_MONTH)
     }
