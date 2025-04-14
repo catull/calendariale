97bb4e8f354f25aede0d3edaaac811e1e9a8a038
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:10:30 +0200

    Fixed lint findings

--- src/calendar/OlympiadCalendar.ts
+++ src/calendar/OlympiadCalendar.ts
@@ -1,35 +1,35 @@
-import { amod } from '../Astro';
-import { INVALID_YEAR, olympiad } from '../Const';
+import { amod } from '../Astro'
+import { INVALID_YEAR, olympiad } from '../Const'
 
-import { JulianCalendar } from './JulianCalendar';
-import { OlympiadDate } from './OlympiadDate';
-import { CalendarDateValidationException } from './core/index';
+import { JulianCalendar } from './JulianCalendar'
+import { OlympiadDate } from './OlympiadDate'
+import { CalendarDateValidationException } from './core/index'
 
 export class OlympiadCalendar {
   // Determine Julian day number (JDN) from Olympiad cycle and year
-  public static toJdn(cycle: number, year: number): number {
-    this.validate(cycle, year);
-    let julianYear = olympiad.EPOCH_JULIAN_YEAR + (cycle - 1) * 4 + year - 1;
+  public static toJdn (cycle: number, year: number): number {
+    this.validate(cycle, year)
+    let julianYear = olympiad.EPOCH_JULIAN_YEAR + (cycle - 1) * 4 + year - 1
 
     if (julianYear >= 0) {
-      julianYear += 1;
+      julianYear += 1
     }
 
-    return JulianCalendar.toJdn(julianYear, 1, 1);
+    return JulianCalendar.toJdn(julianYear, 1, 1)
   }
 
   // Calculate Olympiad calendar date from Julian day number (JDN)
-  public static fromJdn(jdn: number): OlympiadDate {
-    const olympiadYears = Math.floor((jdn - olympiad.EPOCH) / 365.25) + 1;
-    const cycle = 1 + Math.floor((olympiadYears - 1) / 4);
-    const year = amod(olympiadYears, 4);
+  public static fromJdn (jdn: number): OlympiadDate {
+    const olympiadYears = Math.floor((jdn - olympiad.EPOCH) / 365.25) + 1
+    const cycle = 1 + Math.floor((olympiadYears - 1) / 4)
+    const year = amod(olympiadYears, 4)
 
-    return new OlympiadDate(jdn, cycle, year);
+    return new OlympiadDate(jdn, cycle, year)
   }
 
-  private static validate(_cycle: number, year: number): void {
+  private static validate (_cycle: number, year: number): void {
     if (year < 1 || year > 4) {
-      throw new CalendarDateValidationException(INVALID_YEAR);
+      throw new CalendarDateValidationException(INVALID_YEAR)
     }
   }
 }
