627894f9b296bf6af6c2195d0755459385d954f3
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:10:29 +0200

    Fixed lint findings

--- src/calendar/CopticDate.ts
+++ src/calendar/CopticDate.ts
@@ -1,8 +1,8 @@
-import { CopticCalendar } from './CopticCalendar';
-import { LeapDate } from './core/index';
+import { CopticCalendar } from './CopticCalendar'
+import { LeapDate } from './core/index'
 
 export class CopticDate extends LeapDate {
-  constructor(jdn: number, year: number, month: number, day: number) {
-    super(jdn, year, month, day, CopticCalendar.isLeapYear(year));
+  constructor (jdn: number, year: number, month: number, day: number) {
+    super(jdn, year, month, day, CopticCalendar.isLeapYear(year))
   }
 }
