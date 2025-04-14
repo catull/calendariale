bfa1cd930227478d9ab88546665db6e3cf04ba9f
Carlo Dapor <carlo.dapor@mimacom.com>
2025-04-14 19:10:28 +0200

    Fixed lint findings

--- src/calendar/AkanNameCalendar.ts
+++ src/calendar/AkanNameCalendar.ts
@@ -1,14 +1,14 @@
-import { amod } from '../Astro';
-import { akan } from '../Const';
+import { amod } from '../Astro'
+import { akan } from '../Const'
 
-import { AkanNameDate } from './AkanNameDate';
+import { AkanNameDate } from './AkanNameDate'
 
 export class AkanNameCalendar {
   // Calculate Akan Name calendar date from Julian day number (JDN)
-  public static fromJdn(jdn: number): AkanNameDate {
+  public static fromJdn (jdn: number): AkanNameDate {
     // const count: number = jdn - akan.EPOCH;
-    const count: number = jdn - akan.DELTA;
+    const count: number = jdn - akan.DELTA
 
-    return new AkanNameDate(jdn, amod(count, 6), amod(count, 7));
+    return new AkanNameDate(jdn, amod(count, 6), amod(count, 7))
   }
 }
