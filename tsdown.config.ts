792a051aa590b11089721eb04ef52fc621c275e4
catull <catull@gmail.com>
2025-10-27 10:12:20 +0100

    Migrated tsup -> tsdown

---
+++ tsdown.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'tsdown';
+
+export default defineConfig ({
+  clean: true,
+  dts: true,
+  entryPoints: ['src/index.ts'],
+  format: ['esm'],
+  outDir: 'dist',
+  splitting: false,
+  sourcemap: true,
+  target: 'esnext',
+  treeshake: true,
+});
+
