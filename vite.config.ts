2e84370f83600cc67157ac69369bab85463f9087
catull <catull@gmail.com>
2026-07-10 18:13:13 +0200

    New build tool vite-plus.

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,18 @@ import { defineConfig } from "vite-plus";
 
 export default defineConfig({
   build: {
+    chunkImportMap: true,
     lib: {
-      entry: resolve(import.meta.dirname, "src/index.ts"),
-      name: "calendariale",
+      entry: resolve(import.meta.dirname, "src/index.ts"), //["src/index.ts"],
       fileName: "index",
       formats: ["cjs", "es"],
+      name: "calendariale",
     },
+    license: true,
+    manifest: true,
+    minify: "oxc",
+    outDir: "dist",
+    sourcemap: true,
   },
   fmt: {
     arrowParens: "always",
@@ -18,9 +24,11 @@ export default defineConfig({
     semi: true,
     singleQuote: false,
     sortImports: false,
+    sortPackageJson: true,
     trailingComma: "all",
   },
   lint: {
+    ignorePatterns: ["dist/**"],
     options: {
       typeAware: true,
       typeCheck: true,
@@ -31,8 +39,28 @@ export default defineConfig({
       tsgo: true,
     },
     exports: true,
+    format: ["esm", "module"],
+    sourcemap: true,
+  },
+  run: {
+    tasks: {
+      build: ["vp run barro", "vp build"],
+      check: ["vp run lint:check", "vp run build:check", "vp run format:check"],
+      pack: ["vp run build:dev"],
+    },
   },
   staged: {
     "*": "vp check --fix",
+  },
+  test: {
+    coverage: {
+      provider: "v8",
+      enabled: true,
+      clean: true,
+      include: ["src/**/*.ts"],
+      exclude: ["src/**/index.ts"],
+      reporter: ["clover", "cobertura", "html", "json", "lcov", "teamcity", "text"],
+      reportsDirectory: "coverage",
+    },
   },
 });
